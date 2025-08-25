6c3cb976c5009db199e6cbeab4ad55e0eaa4b8bb
Carolina559 <carolina.makoski.alves@escola.pr.gov.br>
2025-08-25 08:52:38 -0300

    fhfhfhmn

---
+++ script.js
@@ -0,0 +1,9 @@
+ const botaoMostraPalaavra= documnto.querySelector("#botao-palavrachave");
+ botaoMostraPalaavra.addEventListener("click",mostraPalavraChave);
+ function mostraPalavraChave(){
+     const texto= documento.querySelector("#entrada-de-texto").valeu;
+     const campoResultado= documento.querySelector("#resultado-palavrachave");
+     const palavrachave= processaTexto(texto);
+     campoResultado.textoContent= palavrachave.join(",");
+ }
+
